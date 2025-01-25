2429bb9723d26226ee017463369bdc738a37d229
RoyPrinsGH <royprinsgm@gmail.com>
2025-01-25 22:16:05 +0100

    Add account page styling

--- OjiSoftPortal/OjiSoftPortalFrontEnd/src/pages/AccountPage.tsx
+++ OjiSoftPortal/OjiSoftPortalFrontEnd/src/pages/AccountPage.tsx
@@ -16,44 +16,59 @@ const AccountPage: React.FC = () => {
             <OjiSoftLogo />
             <br />
             <br />
-            <div className="flex flex-col justify-center">
-                <div className="flex flex-row justify-center mb-2">
-                    <span className="text w-1/4 min-w-60 italic">
-                        --- Name ---
-                    </span>
-                </div>
-                <div className="flex flex-row justify-center mb-2">
-                    <span className="text-blue-500 bg-black whitespace-pre font-mono text-center w-1/4 min-w-60">
-                        {user && user.profile.name}
-                    </span>
-                </div>
-                <br />
-                <div className="flex flex-row justify-center mb-2">
-                    <span className="text w-1/4 min-w-60 italic">
-                        --- Roles ---
-                    </span>
-                </div>
-                <div className="flex flex-row justify-center">
-                    <div className="flex flex-col justify-center">
-                        {user && Object.values(user.profile.role).map(role => (
-                            <div className="text-purple-500 bg-black whitespace-pre font-mono text-center w-1/4 min-w-60">
-                                {role as string}
+            <div className="flex flex-row justify-center gap-4">
+                <div className="flex flex-col w-1/12 gap-4">
+                    <div className="flex flex-row justify-center">
+                        <div className="flex flex-col justify-center border border-green-500 p-2.5 w-full">
+                            <div className="flex flex-row justify-center border-b border-gray-500 mb-2">
+                                <span className="text italic">
+                                    Name
+                                </span>
                             </div>
-                        ))}
+                            <div className="flex flex-row justify-center">
+                                <span className="text-blue-500 whitespace-pre font-mono text-center">
+                                    {user && user.profile.name}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-row justify-center">
+                        <div className="flex flex-col justify-center border border-green-500 p-2.5 w-full">
+                            <div className="flex flex-row justify-center border-b border-gray-500 mb-2">
+                                <span className="text italic">
+                                    Roles
+                                </span>
+                            </div>
+                            {user && Object.values(user.profile.role).map(role => (
+                                <div className="flex flex-row justify-center text-purple-500 whitespace-pre font-mono text-center">
+                                    <span>{role as string}</span>
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 </div>
-                <br />
-                <div className="flex flex-row justify-center mb-2">
-                    <span className="text w-1/4 min-w-60 italic">
-                        --- Actions ---
-                    </span>
-                </div>
-                <div className="flex flex-row justify-center gap-4">
-                    <button className="text button w-1/12 min-w-20" onClick={logout}>Log Out</button>
-                    <button className="text button w-1/12 min-w-20">Edit Profile</button>
-                    <button className="text button w-1/12 min-w-20">Delete Account</button>
+                <div className="flex flex-col w-1/12 gap-4">
+                    <div className="flex flex-row justify-center">
+                        <div className="flex flex-col border border-green-500 p-2.5 w-full gap-2">
+                            <div className="flex flex-row justify-center border-b border-gray-500 mb-2">
+                                <span className="text italic">
+                                    Actions
+                                </span>
+                            </div>
+                            <div className="flex flex-row justify-center">
+                                <button className="text button w-full">Edit Profile</button>
+                            </div>
+                            <div className="flex flex-row justify-center">
+                                <button className="text button w-full" onClick={logout}>Log Out</button>
+                            </div>
+                            <div className="flex flex-row justify-center">
+                                <button className="text button w-full">Delete Account</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+            
         </div>
     );
 }
